f1b3a404bae6111c3a6f48607eff52c7b804902f
Shamael Syed <syedshamael1999@gmail.com>
2025-04-09 17:34:39 +0530

    Update 01_database_exploration.sql

--- scripts/01_database_exploration.sql
+++ scripts/01_database_exploration.sql
@@ -22,9 +22,11 @@ FROM INFORMATION_SCHEMA.TABLES;
 
 
 -- Retrieve all columns for a specific table (dim_customers)
+-- Retrieve all columns for a specific table (dim_customers)
 SELECT 
-      COLUMN_NAME, 
-      DATA_TYPE, 
-      IS_NULLABLE, 
-      CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS;
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
